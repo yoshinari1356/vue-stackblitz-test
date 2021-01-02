c60d45ee4d03b0dc514c7faf0e01c6451a3b3f51
yoshinari1356 <n0802jp2@gmail.com>
2021-01-02 15:47:35 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-stackblitz-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-stackblitz-test)
---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <span>page: {{ page }}</span>
+    <button @click="clickbtn">クリック</button>
+    <HelloWorld msg="HelloWorld" v-if="page == 0" />
+    <New msg="new xxx" v-if="page == 1" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import New from "./components/new.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    New
+  },
+  data() {
+    return {
+      page: 1
+    };
+  },
+  methods: {
+    clickbtn(e) {
+      if (this.page == 1) {
+        this.page = 0;
+      } else {
+        this.page = 1;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
